1e8cdd074bb677a7f781fef8445ec760c6828332
James <jamesgrubb@mac.com>
2020-12-19 15:38:01 +0000

    all

--- amplify/backend/function/handsfunction/src/index.js
+++ amplify/backend/function/handsfunction/src/index.js
@@ -1,13 +1,15 @@
-
+const fetch = require('node-fetch')
 
 exports.handler = async (event) => {
+console.log(event)
+
     // TODO implement
     const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*"
-    //  }, 
+     headers: {
+         "Access-Control-Allow-Origin": "*"
+     }, 
         body: JSON.stringify('Hello from Lambda!'),
     };
     return response;
